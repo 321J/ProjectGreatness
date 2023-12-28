2eb2abe77ab5994a80e12463618673510aed1289
321J <150202229+321J@users.noreply.github.com>
2023-12-28 10:06:07 -0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -3,5 +3,5 @@ local gameId = game.GameId
 if gameId == 3150475059 then
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ae28859a928396a5f09f847895ec4fb9.lua"))()
 elseif gameId == 4777817887 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/321J/ProjectEgoist/main/Egoist.lua"))()
+    loadstring(game:HttpGet("https://github.com/321J/ProjectGreatness/blob/main/Bladeball.lua"))()
 end
